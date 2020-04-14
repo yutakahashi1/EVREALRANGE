919dec7fe503d170f74727d99aa66a280c985db2
yutakahashi1 <federeryuukun1@gmail.com>
2020-04-14 23:31:16 +0900

    organize car function 3
    apply graphs in cars show

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,13 +1,31 @@
 class CarsController < ApplicationController
   def index
     @cars = Car.all
-    @user_data= User.where(payment_result: 1).group("date(created_at)").count
+    
+
+    
 
   end
   
   def show
     @car = Car.find(params[:id])
     @posts = @car.posts
+    
+    @projected_ranges = []
+    @posts.each do |post|
+      @projected_range = (post.distance.to_f/post.consumption.to_f*100).round(1)
+      @projected_ranges << @projected_range 
+    end
+
+    userid = Car.joins(:users).pluck(:nickname)
+    tickInterval = 1
+    post = Post.pluck(:distance)
+    
+    @graph_data = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: 'Projected Range')
+      f.xAxis(categories: "", tickInterval: tickInterval)
+      f.series(name: 'Name', data: @projected_ranges, type: 'scatter')
+    end
   
   end
 end
